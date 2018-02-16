8b402a650c2fb4e11f79bd57cf91e5d26adf6eee
AREL <AREL@B107-02>
2018-02-16 15:43:16 +0300

    deneme

--- WpfApplication1/MainWindow.xaml.cs
+++ WpfApplication1/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace WpfApplication1
         {
             InitializeComponent();
         }
+
+        private void btnTikla_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("butona tıklandı");
+        }
     }
 }
